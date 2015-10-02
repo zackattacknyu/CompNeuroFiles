588d919f9d3515ed41473bbc0fe8ac6d51f318b1
zackattacknyu <zackattacknyu@gmail.com>
2015-10-01 17:59:24 -0700

    more work done

--- HW1/graph17b.m
+++ HW1/graph17b.m
@@ -10,7 +10,9 @@ deltaS = 0.029;
 r_max = 36.03;
 
 %function to generate tuning curve
-ff = @(xx) r_max./(1+exp((s_half-xx)./deltaS));
+%ff = @(xx) r_max./(1+exp((s_half-xx)./deltaS));
+ff = @(xx,gain) (r_max*gain)./(1+exp((s_half-xx)./deltaS));
+%ff = @(xx,gain) r_max./(1+exp((s_half-xx)./(deltaS*gain)));
 
 %varying widths
 width1 = 1;
@@ -19,7 +21,12 @@ width3 = 0.5;
 
 figure
 hold on
-plot(ss,ff(ss),'r-');
+plot(ss,ff(ss,width1),'r-');
+plot(ss,ff(ss,width2),'b-');
+plot(ss,ff(ss,width3),'g-');
+legend('Original Curve',...
+    'Width=2 revolution',...
+    'Width = 1/2 revolution');
 legend('Original Curve');
 hold off
 xlabel('s (movement direction in degrees)');
