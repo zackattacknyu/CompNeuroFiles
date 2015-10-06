5aab5064df604ccee11f4d9e0e8856d305bcc120
zackattacknyu <zackattacknyu@gmail.com>
2015-10-05 21:14:30 -0700

    more work done

--- HW1/problem2script.m
+++ HW1/problem2script.m
@@ -115,5 +115,89 @@ meanByPopSize: 8.5    6.3    5.1    4.3    3.9    3.0    2.2    1.5
 
 Thus, as we increase the number of neurons, the average error does decrease
 %}
+%%
+
+%{
+When the firing rates are kept constant, but the preferred angles
+    vary, there is some error. I started by evenly spacing the 
+    preferred directions and then offset them by a random number in the range
+    -noiseWidth to noiseWidth. As the noiseWidth parameter increased,
+    the error increased. It seems to be minimized
+    at the preferred directions of the neurons
+%}
+minFiringRate = 1;
+maxFiringRate = 1;
+noiseWidth = pi/18;
+popSize=6;
+
+[curves,error,meanError7,xx,preferredAngles,maxRates] = ...
+    problem2function(numAngles,popSize,noiseWidth,...
+    minFiringRate,maxFiringRate );
+
+graphPopulationCodes(xx,curves,preferredAngles,maxRates);
+graphErrors(xx,error);
+
+meanError7
+
+noiseWidth = pi/6;
+
+[curves,error,meanError8,xx,preferredAngles,maxRates] = ...
+    problem2function(numAngles,popSize,noiseWidth,...
+    minFiringRate,maxFiringRate );
+
+graphPopulationCodes(xx,curves,preferredAngles,maxRates);
+graphErrors(xx,error);
+
+meanError8
+
+%{
+When I ran the above part, this was the result:
+
+meanError7=4.9 
+meanError8=11.5
+
+Figure 5 shows the population code graph for 6 neurons
+    where preferred direction is offset by at most pi/18
+Figure 6 shows the graph of direction vs angle error for 6 neurons
+    where preferred direction is offset by at most pi/18
+
+Figure 7 shows the population code graph for 6 neurons
+    where preferred direction is offset by at most pi/6
+Figure 8 shows the graph of direction vs angle error for 6 neurons
+    where preferred direction is offset by at most pi/6
+
+The error seems to increase as we increase the offset from even spacing.
+I proved that out more below
+%}
+%%
+%{
+Here I ran multiple trials for each variance in preferred direction case
+    to see the impact of population size on angle error
+%}
+numTrials = 1000;
+popSize = 8;
+noiseWidths = [pi/18 pi/9 pi/6 pi/3];
+meanByNoiseWidth = zeros(size(noiseWidths));
+for sizeNum=1:length(noiseWidths)
+    noiseWidth = noiseWidths(sizeNum);
+    means = zeros(1,numTrials);
+   for jj = 1:numTrials
+    [~,~,means(jj)] = ...
+        problem2function(numAngles,popSize,noiseWidth,...
+        minFiringRate,maxFiringRate );
+   end
+   meanByNoiseWidth(sizeNum) = mean(means);
+end
+meanByNoiseWidth
+%{
+When I ran the script, I obtained the following values:
+#Neurons:         pi/18  pi/9   pi/6  pi/3
+meanByNoiseWidth: 3.2    6.2    8.9   15.2
+
+This shows that as we increase how much preferred directions are offset,
+    the error increases
+%}
+
+
 
 
