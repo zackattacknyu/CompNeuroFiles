be20b4e53dd8a8433f140c893881832c59654ed8
zackattacknyu <zackattacknyu@gmail.com>
2015-10-05 17:12:41 -0700

    work done

--- HW1/problem2script.m
+++ HW1/problem2script.m
@@ -1,21 +1,27 @@
+%This is the script for problem 2
 
-numAngles = 100;
-noiseWidth = 0;
+%{
+When the preferred angles are evenly spaced,
+    and the firing rates are all constant, the error
+    ends up near zero
+%}
+numAngles = 100; %number of test angles
+noiseWidth = 0; %variance in preferred angles
+popSize=4; %number of neurons
 
-%population size
-popSize=4;
-%popSize=12;
-
+%firing rate range
 minFiringRate = 1;
 maxFiringRate = 1;
 
 [ xx,preferredAngles,maxRates ] = generatePopCodeParameters( ...
     numAngles,popSize,noiseWidth,minFiringRate,maxFiringRate );
-
 [curves,error] = generatePopulationCodes(xx,maxRates,preferredAngles);
-%%
-graphPopulationCodes(xx,curves,preferredAngles,maxRates);
-%%
-graphErrors(xx,error);
+
 
 meanError = radtodeg(mean(error));
+
+graphPopulationCodes(xx,curves,preferredAngles,maxRates);
+
+graphErrors(xx,error);
+
+
