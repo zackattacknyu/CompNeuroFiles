09bde8c642fedb9f19b2adbbe20cf27f315c9fa3
zackattacknyu <zackattacknyu@gmail.com>
2015-10-07 18:35:31 -0700

    more work done

--- HW2/goldman.m
+++ HW2/goldman.m
@@ -11,12 +11,23 @@ ClMinusOut = 125;
 b = 0.02;
 c = 1;
 
-numer = kPlusOut + b*NaPlusOut + c*ClMinusIn;
-denom = kPlusIn + b*NaPlusIn * c*ClMinusOut;
-Em = 58*log10(numer/denom);
+%numer = kPlusOut + b*NaPlusOut + c*ClMinusIn;
+%denom = kPlusIn + b*NaPlusIn * c*ClMinusOut;
+%Em = 58*log10(numer/denom);
 
-Ecl = 58*log10(ClMinusIn/ClMinusOut);
-EK = 58*log10(kPlusOut/kPlusIn);
-Ena = 58*log10(NaPlusOut/NaPlusIn);
+%Ecl = 58*log10(ClMinusIn/ClMinusOut);
+%EK = 58*log10(kPlusOut/kPlusIn);
+%Ena = 58*log10(NaPlusOut/NaPlusIn);
 
-getEm = @(b) 58*log10(numer/denom);
+getEm = @(b) 58*log10(kPlusOut + b*NaPlusOut + c*ClMinusIn...
+    /kPlusIn + b*NaPlusIn * c*ClMinusOut);
+
+emVals = zeros(1,100);
+for i = 1:100
+   emVals(i) = getEm(0.02); 
+end
+
+for i = 45:55
+   emVals(i) = getEm(20); 
+end
+
