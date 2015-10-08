2c58ab23338d687f0fdc43ac0ed38b1e2e55380e
zackattacknyu <zackattacknyu@gmail.com>
2015-10-08 14:48:24 -0700

    finished part 1

--- HW2/goldman.m
+++ HW2/goldman.m
@@ -1,26 +1,18 @@
+function goldman
 
-%initial ion concentrations
-kPlusOut = 5;
-kPlusIn = 125;
-NaPlusOut = 120;
-NaPlusIn = 12;
-ClMinusIn = 5;
-ClMinusOut = 125;
-
-%goldman equation parameters
-b = 0.02;
-c = 1;
-
-%numer = kPlusOut + b*NaPlusOut + c*ClMinusIn;
-%denom = kPlusIn + b*NaPlusIn * c*ClMinusOut;
-%Em = 58*log10(numer/denom);
-
-%Ecl = 58*log10(ClMinusIn/ClMinusOut);
-%EK = 58*log10(kPlusOut/kPlusIn);
-%Ena = 58*log10(NaPlusOut/NaPlusIn);
-
-getEm = @(b) 58*log10(kPlusOut + b*NaPlusOut + c*ClMinusIn...
-    /kPlusIn + b*NaPlusIn * c*ClMinusOut);
+    function em = getEm(b)
+        kPlusOut = 5;
+        kPlusIn = 125;
+        NaPlusOut = 120;
+        NaPlusIn = 12;
+        ClMinusIn = 5;
+        ClMinusOut = 125;
+        c=1;
+        
+        numer = kPlusOut + b*NaPlusOut + c*ClMinusIn;
+        denom = kPlusIn + b*NaPlusIn + c*ClMinusOut;
+        em = 58*log10(numer/denom);
+    end
 
 emVals = zeros(1,100);
 for i = 1:100
@@ -31,3 +23,10 @@ for i = 45:55
    emVals(i) = getEm(20); 
 end
 
+figure
+plot(1:100,emVals);
+title('Membrane Potential during different time steps');
+xlabel('Time Step');
+ylabel('Membrane Potential (mV)');
+
+end
