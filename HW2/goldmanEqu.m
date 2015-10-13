7acc673869900711d6d1422b9c753cc84753e9c1
zackattacknyu <zackattacknyu@gmail.com>
2015-10-13 16:13:09 -0700

    finished part1

---
+++ HW2/goldmanEqu.m
@@ -0,0 +1,8 @@
+%{
+This plots the membrane potential values using the Goldman equation.
+When the permeability ratio is 0.02 (time steps 1-44 and 56-100), 
+    the membrane potential is around -75.7 mV
+When the permeability ratio is 20 (time steps 45-55), 
+    the membrane potential increases to around 40.13 mV
+%}
+goldman;
--- HW2/ifmodel.m
+++ HW2/ifmodel.m
@@ -1,2 +1,10 @@
-iVals = 0:0.05:4;
-ifNeuronPlot( 0:0.05:4 )
+%shows the frequency vs current in the 0-4 nA range
+%   the neuron starts firing at 1.5 nA and the firing rate increases
+%   rapidly at first, followed by increasing in a step-like manner
+ifNeuronPlot( 0:0.05:4 );
+
+%shows the frequency vs current in the 0-20 nA range
+%   The firing rate increases in a step-like manner with the steps
+%   growing larger and larger. At 16 nA, it reaches the maximal
+%   firing rate of 2000 Hz that we can calculate
+ifNeuronPlot( 0:.1:20 );
