9d1f9858c1914d53f2258a5a7e6d3238b491da34
zackattacknyu <zackattacknyu@gmail.com>
2015-11-06 18:47:40 -0800

    almost working

--- Midterm/problem2.m
+++ Midterm/problem2.m
@@ -1,5 +1,6 @@
 clear all;
 
+%number of CA3 neurons
 numCA3 = 100;
 
 %parameters from izzynet code
@@ -9,53 +10,79 @@ cc = -65;
 dd = 8;
 
 % STDP lists for LTD and LTP weight changes
-LTP = 0;    % from CA3 to CA1 neurons
+LTP = zeros(1,numCA3);    % from CA3 to CA1 neurons
 LTD = zeros(1,numCA3);    % from CA3 to CA1 neurons
 
 % STDP parameters
-%A_plus = 0.1;
-A_plus = 0.05;
+A_plus = 0.1;
 A_minus = -0.105;
 t_plus = 20;
 t_minus = 20;
 
 wmin = 0;
 wmax = 5.0;
+%wmax = 30.0;
 
-sigma=3;
+sigma=1; %sigma for place field Gaussians
+sigmaStrength = 10; %sigma for strength Gaussian curve
+
 maxCurrent = 30;
 
 %CA1 neuron
 v1 = cc;
 u1 = bb*v1;
 
+%CA3 neurons
 numFirings = zeros(1,100);
 
+%maximum initial weight
+maxInitWeight = 30;
 
-gaussWeights = normpdf(1:100,50,18);
-gaussWeights = gaussWeights*(1/max(gaussWeights));
-gaussWeights = gaussWeights*2;
+%max init current for CA3 neurons
+maxInitCurrent = 30;
 
-%strength = 2*rand(1,numCA3);
-strength = gaussWeights;
 
-for step = 1:100
+gaussWeights = normpdf(1:100,50,sigmaStrength);
+gaussWeights = gaussWeights*(1/max(gaussWeights)); %sets max to 1
+strength = gaussWeights*maxInitWeight;
+%strength = gaussWeights*100;
+%strength = gaussWeights*wmax;
+
+vv = -65*ones(1,numCA3);
+uu = bb.*vv;
+
+numLaps = 17;
+skewValues = zeros(1,numLaps);
+
+for lap = 1:numLaps
     
-    %I1 = gaussWeights(step)*10;
-   
-    for t = 1:1000
+    %LTP = zeros(1,numCA3);    % from CA3 to CA1 neurons
+    %LTD = zeros(1,numCA3);    % from CA3 to CA1 neurons
+    
+    for step = 1:100
         
+        I = normpdf(step,1:100,sigma);
+        I = I/(max(I)); %sets max to 1
+        I = I*maxInitCurrent; %sets max current to 30 so neuron will fire
+        
+        %I1 = gaussWeights(step)*10;
+        
+        %update CA3 neuron voltages
+        vv=vv+0.5.*(0.04.*vv.^2+5.*vv+140-uu+I);
+        vv=vv+0.5.*(0.04.*vv.^2+5.*vv+140-uu+I);
+        uu=uu+aa.*(bb.*vv-uu);
+
+        fired = find(vv>=30);
+
         %CA1 neuron current
         % adds strengths of ones that fired
-        I1 = strength(step)*20;
+        I1 = abs(2*randn(1,1)) + sum(strength(fired));
         
-        
-        %CA1 neuron voltage
-        if(v1>=30)
-            v1 = cc;
-            u1 = u1+dd;
-            numFirings(step) = numFirings(step)+1;
-            
+        %update CA3 neurons that fired
+        if ~isempty(fired)
+            vv(fired)=cc;
+            uu(fired)=uu(fired)+dd;
+                
             % if neuron i is excitatory
             % 1)    apply LTP for weights that connect to neuron i 
             %       (i.e., the neurons that fired prior to i)  
@@ -64,29 +91,52 @@ for step = 1:100
             % 3)    set the max LTP for  weights that connect from neuron i
             % 4)    set the max LTD for weights that connect to neuron i
 
+            strength(fired) = max(wmin,strength(fired) + LTD(fired)); % LTP to weights that connect to i from all exc 
+            LTP(fired) = A_plus;
+
             
-            strength(step) = max(wmin,strength(step) + LTD(step)); % LTD to weights that connect to all exc from i
-            LTD(step) = A_minus;  % set max LTD to i from all exc
-            
-        end
+        end;
         
-        strength(step) = min(wmax,strength(step) + LTP); % LTP to weights that connect to i from all exc 
-        LTP = A_plus;   % set max LTP to all exc from i
-        
+        %update CA1 neuron voltages
         v1=v1+0.5*(0.04*v1^2+5*v1+140-u1+I1);
         v1=v1+0.5*(0.04*v1^2+5*v1+140-u1+I1);
         u1=u1+aa*(bb*v1-u1);
-        
-        
-        
+
+
+        %CA1 neuron voltage
+        if(v1>=30)
+            v1 = cc;
+            u1 = u1+dd;
+            numFirings(step) = numFirings(step)+1;
+
+            % if neuron i is excitatory
+            % 1)    apply LTP for weights that connect to neuron i 
+            %       (i.e., the neurons that fired prior to i)  
+            % 2)    apply LTD for weights from neuron i 
+            %       (i.e., the neurons that i fired before) 
+            % 3)    set the max LTP for  weights that connect from neuron i
+            % 4)    set the max LTD for weights that connect to neuron i
+
+            strength = min(wmax,strength + LTP); % LTP to weights that connect to i from all exc 
+            LTD(:) = A_minus;
+
+        end
+
         % exponentially decay LTD and LTP based on time constants
         LTP = LTP - LTP/t_plus;
         LTD = LTD - LTD/t_minus;
+
+    end
+    
+    if(lap<2)
+    
+        figure
+        plot(numFirings)
+        
         
     end
     
-    
-    
+    skewValues(lap) = skewness(strength);
 end
 
 figure
@@ -94,3 +144,6 @@ plot(numFirings)
 
 figure
 plot(strength)
+
+figure
+plot(skewValues)
