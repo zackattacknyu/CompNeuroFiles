36ec95700e20525192d0372840bc0e7b545cec81
zackattacknyu <zackattacknyu@gmail.com>
2015-11-08 16:31:39 -0800

    made submission file for prob2

--- Midterm/myskewness.m
+++ Midterm/myskewness.m
@@ -1,13 +1,23 @@
 function [ skew ] = myskewness( X )
-%MYSKEWNESS Summary of this function goes here
-%   Detailed explanation goes here
+%MYSKEWNESS Calculates skewness for problem 2
+%
+%Because we wanted to figure out how much the values were skewed toward the
+% left side rather than the right side, I decided to make all the left side
+% values negative and the right side values positive. 
+%
+%I also made the mu value in the skewness equation 0 to further enforce the
+% fact that we want to see how the values are skewed in favor of the left
+% side
+%
 
 lenX = length(X);
-X(1:lenX/2)=-1*X(1:lenX/2);
+X(1:lenX/2)=-1*X(1:lenX/2); %makes first 50 values negative
 
+%{
+Calculate skewness as per skewness.m code in Matlab except mu=0
+%}
 numer = mean(X.^3);
 denom = std(X)^3;
-
 skew = numer/denom;
 
 end
--- Midterm/submission/midtermProb2.m
+++ Midterm/submission/midtermProb2.m
@@ -1,3 +1,5 @@
+function [] = midtermProb2(  )
+
 %{
 This is my code for Problem 2
 
@@ -303,3 +305,32 @@ xlabel('Lap Number');
 ylabel('Location of Spike During Lap (neuron step)');
 legend('First Spike','Last Spike','Location','eastoutside');
 hold off
+
+function [ skew ] = myskewness( X )
+%MYSKEWNESS Calculates skewness for problem 2
+%
+%Because we wanted to figure out how much the values were skewed toward the
+% left side rather than the right side, I decided to make all the left side
+% values negative and the right side values positive. 
+%
+%I also made the mu value in the skewness equation 0 to further enforce the
+% fact that we want to see how the values are skewed in favor of the left
+% side
+%
+
+lenX = length(X);
+X(1:lenX/2)=-1*X(1:lenX/2); %makes first 50 values negative
+
+%{
+Calculate skewness as per skewness.m code in Matlab except mu=0
+%}
+numer = mean(X.^3);
+denom = std(X)^3;
+skew = numer/denom;
+
+end
+
+
+
+end
+
