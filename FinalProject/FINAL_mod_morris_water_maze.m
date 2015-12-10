f9ab6a97c7f66735971db0880d38e363769076b1
zackattacknyu <zackattacknyu@gmail.com>
2015-12-09 19:38:28 -0800

    work done

--- FinalProject/FINAL_mod_morris_water_maze.m
+++ FinalProject/FINAL_mod_morris_water_maze.m
@@ -39,4 +39,40 @@ FINAL_make2figure( rewards,z,ratPathsX,ratPathsY )
 
 %%
 
+[rewards,~] = FINAL_getGeoRewards([1 2 3 4 5],0.7);
+
+%rewards = [citiesLong citiesLat];
+%reward_vals = citiesRew;
+reward_vals = ones(size(rewards,1),1); %values for each reward, which decrease. 
+
+%for European cities
+rewardDecFactor = 0.9; %factor to decrease reward value by each time
+rewardDecValue = 0;
+
+epsilonThresh = 0.2;
+
+%TRIALS =20;
+TRIALS =40; %for American cities
+
+[ w,z ] = FINAL_trainRat( rewards,reward_vals,...
+    rewardDecFactor,rewardDecValue, TRIALS, epsilonThresh, 0 );
+
+%runs trials without updating weights
+%records the paths that the rat takes
+
+%TRIALS2 = 15;
+TRIALS2 = 50; %for American cities, 1
+
+[ratPathsX,ratPathsY] = FINAL_testRat(z,TRIALS2);
+
+%%
+
+%Generates Figure 3
+%Won't be exactly the same, but the same parameters were used
+FINAL_make3figure( rewards,z,ratPathsX,ratPathsY )
+
+%Generates Figure 4
+%Won't be exactly the same, but the same parameters were used
+FINAL_makeWfigure( w )
+
 
