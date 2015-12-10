e79547ca5f8ba3defddb8bca3316a0afc9c1d2a4
zackattacknyu <zackattacknyu@gmail.com>
2015-12-09 20:33:42 -0800

    work

--- FinalProject/FINAL_make2figure.m
+++ FinalProject/FINAL_make2figure.m
@@ -1,11 +1,15 @@
 function [ ] = FINAL_make2figure( rewards,z,ratPathsX,ratPathsY )
-%FINAL_MAKE3FIGURE Summary of this function goes here
-%   Detailed explanation goes here
-numRewards = size(rewards,1);
-radius = 1.0; % arena is 2 meters wide
-sigma = 0.16; % place cell tuning width of 0.16m
+%FINAL_MAKE3FIGURE Make the 2-panel figure 5 and 7
+%
+%INPUT
+%   rewards - reward centers
+%   z - actor values
+%   ratPathsX - paths rat took, x coordinates
+%   ratPathsY - paths rat took, y coordinates
 
 % set up place cell indices across circular arena
+radius = 1.0; % arena is 2 meters wide
+sigma = 0.16; % place cell tuning width of 0.16m
 inx = 0;
 for i = -radius:sigma/2:radius
     for j = -radius:sigma/2:radius
@@ -19,10 +23,13 @@ end
 
 figure
 
+%makes figure of actor gradients
 subplot(211)
 vectorPlot(z,pc);
 axis square;
-TRIALS2 = length(ratPathsX);
+
+%color order
+%ensures differently colored lines if using older version of Matlab
 co = [0    0.4470    0.7410;
     0.8500    0.3250    0.0980;
     0.9290    0.6940    0.1250;
@@ -30,9 +37,11 @@ co = [0    0.4470    0.7410;
     0.4660    0.6740    0.1880;
     0.3010    0.7450    0.9330;
     0.6350    0.0780    0.1840];
+
+%plots the paths the rat took superimposed with reward centers
 subplot(212)
 hold on
-for i = 1:TRIALS2
+for i = 1:length(ratPathsX)
     colorRow = mod(i,size(co,1))+1;
    plot(ratPathsX{i},ratPathsY{i},'--','Color',co(colorRow,:)); 
 end
