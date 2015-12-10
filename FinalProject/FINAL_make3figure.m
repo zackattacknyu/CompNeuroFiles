90df66c43b02e69f277142fd81806329d1f89469
zackattacknyu <zackattacknyu@gmail.com>
2015-12-09 20:30:38 -0800

    wor

--- FinalProject/FINAL_make3figure.m
+++ FinalProject/FINAL_make3figure.m
@@ -1,11 +1,17 @@
 function [ ] = FINAL_make3figure( rewards,z,ratPathsX,ratPathsY )
-%FINAL_MAKE3FIGURE Summary of this function goes here
-%   Detailed explanation goes here
+%FINAL_MAKE3FIGURE Make the 3-panel figure 1 and 3
+%  
+%INPUT
+%   rewards - reward centers
+%   z - actor values
+%   ratPathsX - paths rat took, x coordinates
+%   ratPathsY - paths rat took, y coordinates
+
 numRewards = size(rewards,1);
-radius = 1.0; % arena is 2 meters wide
-sigma = 0.16; % place cell tuning width of 0.16m
 
 % set up place cell indices across circular arena
+radius = 1.0; % arena is 2 meters wide
+sigma = 0.16; % place cell tuning width of 0.16m
 inx = 0;
 for i = -radius:sigma/2:radius
     for j = -radius:sigma/2:radius
@@ -17,11 +23,11 @@ for i = -radius:sigma/2:radius
     end
 end
 
+%makes dot and reward center figure
 figure
 subplot(311)
 scatter(pc.x,pc.y,10,'b.')
 hold on
-%scatter(rat.x, rat.y,50,'ro')
 for rNum = 1:numRewards
     reward = rewards(rNum,:);
     scatter(reward(1),reward(2),100,'go')
@@ -29,12 +35,13 @@ end
 axis square;
 hold off
 
+%makes figure of actor gradients
 subplot(312)
 vectorPlot(z,pc);
 axis square;
 
-TRIALS2 = length(ratPathsX);
-
+%color order
+%ensures differently colored lines if using older version of Matlab
 co = [0    0.4470    0.7410;
     0.8500    0.3250    0.0980;
     0.9290    0.6940    0.1250;
@@ -42,9 +49,11 @@ co = [0    0.4470    0.7410;
     0.4660    0.6740    0.1880;
     0.3010    0.7450    0.9330;
     0.6350    0.0780    0.1840];
+
+%plots the paths the rat took superimposed with reward centers
 subplot(313)
 hold on
-for i = 1:TRIALS2
+for i = 1:length(ratPathsX)
     colorRow = mod(i,size(co,1))+1;
    plot(ratPathsX{i},ratPathsY{i},'--','Color',co(colorRow,:)); 
 end
